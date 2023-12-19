4a495411eef397b699e601b5be3115068c6519bc
Uniswap Labs Service Account <hello-happy-puppy@users.noreply.github.com>
2023-12-19 18:37:16 +0000

    ci(release): publish latest release

--- apps/web/src/components/NavBar/index.tsx
+++ apps/web/src/components/NavBar/index.tsx
@@ -1,82 +1,103 @@
-import { Trans } from '@lingui/macro'
-import { useWeb3React } from '@web3-react/core'
-import clsx, { ClassValue } from 'clsx'
-import { useAccountDrawer } from 'components/AccountDrawer'
-import { UniIcon } from 'components/Logo/UniIcon'
-import Web3Status from 'components/Web3Status'
-import { useInfoExplorePageEnabled } from 'featureFlags/flags/infoExplore'
-import { chainIdToBackendName } from 'graphql/data/util'
-import { useDisableNFTRoutes } from 'hooks/useDisableNFTRoutes'
-import { useIsNftPage } from 'hooks/useIsNftPage'
-import { useIsPoolsPage } from 'hooks/useIsPoolsPage'
-import { Box } from 'nft/components/Box'
-import { Row } from 'nft/components/Flex'
-import { useProfilePageState } from 'nft/hooks'
-import { ProfilePageStateType } from 'nft/types'
-import { ReactNode, useCallback } from 'react'
-import { NavLink, NavLinkProps, useLocation, useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
+import { Trans } from "@lingui/macro";
+import { useWeb3React } from "@web3-react/core";
+import clsx, { ClassValue } from "clsx";
+import { useAccountDrawer } from "components/AccountDrawer/MiniPortfolio/hooks";
+import { UniIcon } from "components/Logo/UniIcon";
+import Web3Status from "components/Web3Status";
+import { useInfoExplorePageEnabled } from "featureFlags/flags/infoExplore";
+import { chainIdToBackendName } from "graphql/data/util";
+import { useDisableNFTRoutes } from "hooks/useDisableNFTRoutes";
+import { useIsNftPage } from "hooks/useIsNftPage";
+import { useIsPoolsPage } from "hooks/useIsPoolsPage";
+import { Box } from "nft/components/Box";
+import { Row } from "nft/components/Flex";
+import { useProfilePageState } from "nft/hooks";
+import { ProfilePageStateType } from "nft/types";
+import { ReactNode, useCallback } from "react";
+import {
+  NavLink,
+  NavLinkProps,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import styled from "styled-components";
 
-import { useIsNavSearchInputVisible } from '../../nft/hooks/useIsNavSearchInputVisible'
-import { Bag } from './Bag'
-import Blur from './Blur'
-import { ChainSelector } from './ChainSelector'
-import { MenuDropdown } from './MenuDropdown'
-import { SearchBar } from './SearchBar'
-import * as styles from './style.css'
+import { useIsNavSearchInputVisible } from "../../nft/hooks/useIsNavSearchInputVisible";
+import { Bag } from "./Bag";
+import Blur from "./Blur";
+import { ChainSelector } from "./ChainSelector";
+import { MenuDropdown } from "./MenuDropdown";
+import { SearchBar } from "./SearchBar";
+import * as styles from "./style.css";
 
 const Nav = styled.nav`
   padding: ${({ theme }) => `${theme.navVerticalPad}px 12px`};
   width: 100%;
   height: ${({ theme }) => theme.navHeight}px;
   z-index: 2;
-`
+`;
 
 interface MenuItemProps {
-  href: string
-  id?: NavLinkProps['id']
-  className?: ClassValue
-  isActive?: boolean
-  children: ReactNode
-  dataTestId?: string
+  href: string;
+  id?: NavLinkProps["id"];
+  className?: ClassValue;
+  isActive?: boolean;
+  children: ReactNode;
+  dataTestId?: string;
 }
 
-const MenuItem = ({ href, dataTestId, id, className, isActive, children }: MenuItemProps) => {
+const MenuItem = ({
+  href,
+  dataTestId,
+  id,
+  className,
+  isActive,
+  children,
+}: MenuItemProps) => {
   return (
     <NavLink
       to={href}
-      className={clsx(isActive ? styles.activeMenuItem : styles.menuItem, className)}
+      className={clsx(
+        isActive ? styles.activeMenuItem : styles.menuItem,
+        className
+      )}
       id={id}
-      style={{ textDecoration: 'none' }}
+      style={{ textDecoration: "none" }}
       data-testid={dataTestId}
     >
       {children}
     </NavLink>
-  )
-}
+  );
+};
 
 export const PageTabs = () => {
-  const { pathname } = useLocation()
-  const { chainId: connectedChainId } = useWeb3React()
-  const chainName = chainIdToBackendName(connectedChainId)
+  const { pathname } = useLocation();
+  const { chainId: connectedChainId } = useWeb3React();
+  const chainName = chainIdToBackendName(connectedChainId);
 
-  const isPoolActive = useIsPoolsPage()
-  const isNftPage = useIsNftPage()
+  const isPoolActive = useIsPoolsPage();
+  const isNftPage = useIsNftPage();
 
-  const shouldDisableNFTRoutes = useDisableNFTRoutes()
-  const infoExplorePageEnabled = useInfoExplorePageEnabled()
+  const shouldDisableNFTRoutes = useDisableNFTRoutes();
+  const infoExplorePageEnabled = useInfoExplorePageEnabled();
 
   return (
     <>
-      <MenuItem href="/swap" isActive={pathname.startsWith('/swap')}>
+      <MenuItem href="/swap" isActive={pathname.startsWith("/swap")}>
         <Trans>Swap</Trans>
       </MenuItem>
       {infoExplorePageEnabled ? (
-        <MenuItem href={`/explore/tokens/${chainName.toLowerCase()}`} isActive={pathname.startsWith('/explore')}>
+        <MenuItem
+          href={`/explore/tokens/${chainName.toLowerCase()}`}
+          isActive={pathname.startsWith("/explore")}
+        >
           <Trans>Explore</Trans>
         </MenuItem>
       ) : (
-        <MenuItem href={`/tokens/${chainName.toLowerCase()}`} isActive={pathname.startsWith('/tokens')}>
+        <MenuItem
+          href={`/tokens/${chainName.toLowerCase()}`}
+          isActive={pathname.startsWith("/tokens")}
+        >
           <Trans>Tokens</Trans>
         </MenuItem>
       )}
@@ -85,33 +106,38 @@ export const PageTabs = () => {
           <Trans>NFTs</Trans>
         </MenuItem>
       )}
-      <MenuItem href="/pools" className={styles.pools} dataTestId="pool-nav-link" isActive={isPoolActive}>
+      <MenuItem
+        href="/pools"
+        className={styles.pools}
+        dataTestId="pool-nav-link"
+        isActive={isPoolActive}
+      >
         <Trans>Pools</Trans>
       </MenuItem>
       <Box marginY="4">
         <MenuDropdown />
       </Box>
     </>
-  )
-}
+  );
+};
 
 const Navbar = ({ blur }: { blur: boolean }) => {
-  const isNftPage = useIsNftPage()
-  const sellPageState = useProfilePageState((state) => state.state)
-  const navigate = useNavigate()
-  const isNavSearchInputVisible = useIsNavSearchInputVisible()
+  const isNftPage = useIsNftPage();
+  const sellPageState = useProfilePageState((state) => state.state);
+  const navigate = useNavigate();
+  const isNavSearchInputVisible = useIsNavSearchInputVisible();
 
-  const [accountDrawerOpen, toggleAccountDrawer] = useAccountDrawer()
+  const [accountDrawerOpen, toggleAccountDrawer] = useAccountDrawer();
 
   const handleUniIconClick = useCallback(() => {
     if (accountDrawerOpen) {
-      toggleAccountDrawer()
+      toggleAccountDrawer();
     }
     navigate({
-      pathname: '/',
-      search: '?intro=true',
-    })
-  }, [accountDrawerOpen, navigate, toggleAccountDrawer])
+      pathname: "/",
+      search: "?intro=true",
+    });
+  }, [accountDrawerOpen, navigate, toggleAccountDrawer]);
 
   return (
     <>
@@ -129,30 +155,35 @@ const Navbar = ({ blur }: { blur: boolean }) => {
               />
             </Box>
             {!isNftPage && (
-              <Box display={{ sm: 'flex', lg: 'none' }}>
+              <Box display={{ sm: "flex", lg: "none" }}>
                 <ChainSelector leftAlign={true} />
               </Box>
             )}
-            <Row display={{ sm: 'none', lg: 'flex' }}>
+            <Row display={{ sm: "none", lg: "flex" }}>
               <PageTabs />
             </Row>
           </Box>
           <Box
             className={styles.searchContainer}
             {...(isNavSearchInputVisible && {
-              display: 'flex',
+              display: "flex",
             })}
           >
             <SearchBar />
           </Box>
           <Box className={styles.rightSideContainer}>
             <Row gap="12">
-              <Box position="relative" display={isNavSearchInputVisible ? 'none' : { sm: 'flex' }}>
+              <Box
+                position="relative"
+                display={isNavSearchInputVisible ? "none" : { sm: "flex" }}
+              >
                 <SearchBar />
               </Box>
-              {isNftPage && sellPageState !== ProfilePageStateType.LISTING && <Bag />}
+              {isNftPage && sellPageState !== ProfilePageStateType.LISTING && (
+                <Bag />
+              )}
               {!isNftPage && (
-                <Box display={{ sm: 'none', lg: 'flex' }}>
+                <Box display={{ sm: "none", lg: "flex" }}>
                   <ChainSelector />
                 </Box>
               )}
@@ -163,7 +194,7 @@ const Navbar = ({ blur }: { blur: boolean }) => {
         </Box>
       </Nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
