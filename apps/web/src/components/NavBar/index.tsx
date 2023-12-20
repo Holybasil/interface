8d30a53f8f23cc008c347961a6564f238608e385
Uniswap Labs Service Account <hello-happy-puppy@users.noreply.github.com>
2023-12-20 17:42:07 +0000

    ci(release): publish latest release

--- apps/web/src/components/NavBar/index.tsx
+++ apps/web/src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 import { Trans } from "@lingui/macro";
 import { useWeb3React } from "@web3-react/core";
 import clsx, { ClassValue } from "clsx";
-import { useAccountDrawer } from "components/AccountDrawer/MiniPortfolio/hooks";
+import { useAccountDrawer } from "components/AccountDrawer";
 import { UniIcon } from "components/Logo/UniIcon";
 import Web3Status from "components/Web3Status";
 import { useInfoExplorePageEnabled } from "featureFlags/flags/infoExplore";
